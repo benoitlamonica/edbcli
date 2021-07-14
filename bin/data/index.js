6fdbe0244194b2cc2c7933ff2d578cc8de263db6
Benoit Lamonica <ben33127@gmail.com>
2021-07-14 14:04:44 +0200

    Removed Node module

--- bin/data/index.js
+++ bin/data/index.js
@@ -2,13 +2,34 @@ module.exports = {
     gitRepo: 'https://github.com/benoitlamonica/easydiscordbot.git',
     cmdHeader: `// Automaticaly generated on the ${new Date().toLocaleDateString('fr-FR')}`,
     cmdBody: {
+        bodyWithArgs: `module.exports = {
+    name: '****',
+    description: 'Sample description',
+    execute: (msg, arg) => {
+        new ReponseBot(msg).useCommandHandler('handlerName', {arg: arg});
+    }
+}`,
         body: `module.exports = {
-                name: '****',
-                description: 'Sample description',
-                execute: (msg, arg) => {
-                    new ReponseBot(msg).useCommandHandler('handlerName', arg);
-                }
-            }`,
+    name: '****',
+    description: 'Sample description',
+    execute: (msg, arg) => {
+        new ReponseBot(msg).useCommandHandler('handlerName');
+    }
+}`,
+        bodyAsync: `module.exports = {
+    name: '****',
+    description: 'Sample description',
+    execute: (msg, arg) => {
+        new ReponseBot(msg).useCommandHandler('handlerName', {async: true});
+    }
+}`,
+        bodyAsyncWithArgs: `module.exports = {
+    name: '****',
+    description: 'Sample description',
+    execute: (msg, arg) => {
+        new ReponseBot(msg).useCommandHandler('handlerName', {arg: arg, async: true});
+    }
+}`,
         require: `const { ReponseBot } = require("../../vendor/config/response")`
     }
 }
